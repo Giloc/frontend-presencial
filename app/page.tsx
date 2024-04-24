74000487c7b6b33e8914bfc54ef3392e499e84b7
Giloc Delgado <giloc.delgado@innovati.com.co>
2024-04-23 19:17:44 -0500

    delete ver vuelos

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next';
-import VerVuelos from '../app/BusquedaA/VerVuelos';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <VerVuelos />
     </div>
   );
 };
